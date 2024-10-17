e01121db1ce6349642f22ff89078da77aa74d9a2
litonshil <litonshil009@gmail.com>
2024-10-17 16:15:46 +0600

    comment test-1 (3)

--- infra/http/middlewares/user.go
+++ infra/http/middlewares/user.go
@@ -46,6 +46,8 @@ func GenerateMetadata(c echo.Context, user *domain.User) *domain.User {
 		//	Email:     user.Email,
 		//},
 		//Payload: body,
+
+		//Payload: body,
 	}
 	user.Metadata = metadata
 	return user
